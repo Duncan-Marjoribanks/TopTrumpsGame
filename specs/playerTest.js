e175426b713ae10eec35e1f4ec8470e2f01a5edb
Duncan <marjoribanks.d@gmail.com>
2018-06-19 15:46:15 +0100

    initial commit

---
+++ specs/playerTest.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const Player = require('../player.js');
+const Card = require('../card.js');
+describe('Player', function(){
+
+let player;
+let card;
+
+beforeEach(function(){
+  player = new Player();
+  card = new Card('Superman', 6, 9, 7);
+});
+
+it('should have cards', function(){
+  player.take(card);
+  const actualCards = player.hand;
+  assert.deepStrictEqual(actualCards, [card]);
+})
+
+it('should be able to lose a card', function(){
+  player.take(card);
+  player.loseCard(card);
+  const actualHandSize = player.hand.length;
+  assert.strictEqual(actualHandSize, 0);
+})
+
+
+})
